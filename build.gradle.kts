8193d008e58eb8388b7e3f46bae79dd2f895df13
k.mordvinova <mordvinovacamilla@gmail.com>
2025-12-17 11:57:27 +0300

    metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,31 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+allprojects {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+
+            freeCompilerArgs.addAll(
+                listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                            layout.buildDirectory.asFile.get().absolutePath + "/ compose_metrics",
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                            layout.buildDirectory.asFile.get().absolutePath + "/ compose_metrics"
+                )
+            )
+        }
+    }
+}
+
+
 
